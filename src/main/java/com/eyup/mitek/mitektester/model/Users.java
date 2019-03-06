5aafc8b450cfdd5e8b1ace7bb0ba02707f8f1146
ecingel <ecingel@miteksystems.com>
2019-03-06 11:45:26 +0100

    Update Users.java

--- src/main/java/com/eyup/mitek/mitektester/model/Users.java
+++ src/main/java/com/eyup/mitek/mitektester/model/Users.java
@@ -11,6 +11,8 @@ public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+  
     private String name;
     private String teamName;
     private Integer salary;
